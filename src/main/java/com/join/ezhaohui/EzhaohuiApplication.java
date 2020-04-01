d2e66fef0a7e18d00788ce826f5f7001ab0860a9
Maxin24 <1157564782@qq.com>
2020-04-01 21:57:52 +0800

    Initial commit

---
+++ src/main/java/com/join/ezhaohui/EzhaohuiApplication.java
@@ -0,0 +1,13 @@
+package com.join.ezhaohui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzhaohuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzhaohuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/join/ezhaohui/EzhaohuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.ezhaohui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzhaohuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
